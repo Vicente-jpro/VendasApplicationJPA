1abd790aef7e32c5b03dcef68fe5d98bef677bf4
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-01-13 23:15:55 +0100

    hello end point added

--- src/main/java/com/vendas/controller/HelloController.java
+++ src/main/java/com/vendas/controller/HelloController.java
@@ -1,16 +1,20 @@
 package com.vendas.controller;
 
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/hello")
 public class HelloController {
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello from Spring boot" ;
+	@ResponseBody
+	@GetMapping( value = "/{nome}")
+	public String hello(@PathVariable  String nome ) {
+		return String.format( "Hello %s", nome);
 	}
 	
 	
