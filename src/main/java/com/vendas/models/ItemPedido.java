86d1f2388d5a70f65a016f94871adf39bdbb7515
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-01-17 23:26:55 +0100

    imports removed

--- src/main/java/com/vendas/models/ItemPedido.java
+++ src/main/java/com/vendas/models/ItemPedido.java
@@ -11,10 +11,7 @@ import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 
 @Data
